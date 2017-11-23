2aac2701128a9a2d7e8459323e7564e546830b3a
Peter D. Gray <peter@conalgo.com>
2017-11-23 15:50:24 -0500

    Bogus half-support for BTC/satoshi conversions

--- pycoin/convention/__init__.py
+++ pycoin/convention/__init__.py
@@ -1,29 +1,39 @@
 
-import decimal
+try:
+    from decimal import Decimal as D
 
-SATOSHI_PER_COIN = decimal.Decimal(int(1e8))
-COIN_PER_SATOSHI = decimal.Decimal(1)/SATOSHI_PER_COIN
+    COIN_PER_SATOSHI = D(1)/SATOSHI_PER_COIN
 
-SATOSHI_TO_MBTC = decimal.Decimal(int(1e5))
+except ImportError:
+    # micropython lacks decimal module, and often, floats.
+    def D(n):
+        if isinstance(n, int):
+            return n
+        raise NotImplementedError
+
+    COIN_PER_SATOSHI = NotImplemented
+
+SATOSHI_PER_COIN = D(int(1e8))
+SATOSHI_TO_MBTC = D(int(1e5))
 
 
 def satoshi_to_btc(satoshi_count):
     if satoshi_count == 0:
-        return decimal.Decimal(0)
+        return D(0)
     r = satoshi_count * COIN_PER_SATOSHI
     return r.normalize()
 
 
 def btc_to_satoshi(btc):
-    return int(decimal.Decimal(btc) * SATOSHI_PER_COIN)
+    return int(D(btc) * SATOSHI_PER_COIN)
 
 
 def satoshi_to_mbtc(satoshi_count):
     if satoshi_count == 0:
-        return decimal.Decimal(0)
+        return D(0)
     r = satoshi_count / SATOSHI_TO_MBTC
     return r.normalize()
 
 
 def mbtc_to_satoshi(btc):
-    return int(decimal.Decimal(btc) * SATOSHI_TO_MBTC)
+    return int(D(btc) * SATOSHI_TO_MBTC)
