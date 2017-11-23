a85a4d5a9a02910145d51a91c04a04d4ae1c5a30
Peter D. Gray <peter@conalgo.com>
2017-11-23 10:10:55 -0500

    Move to unittest class

--- tests/test_pure_ripemd160.py
+++ tests/test_pure_ripemd160.py
@@ -4,17 +4,20 @@ import hashlib
 from pycoin.contrib.ripemd import new as ripemd160
 from pycoin.contrib.ripemd import test_ripemd as selftest
 
-def test_ripemd_knowns():
-    selftest()
+class RipemdTest(unittest.TestCase):
+    def test_ripemd_knowns(self):
+        selftest()
 
-def test_ripemd_vs_stdlib():
-    try:
-        hashlib.new('ripemd160')
-    except ValueError:
-        # GAE and micropython may not have it native
-        return
+    def test_ripemd_vs_stdlib(self):
+        try:
+            hashlib.new('ripemd160')
+        except ValueError:
+            # GAE and micropython may not have it native
+            return
 
-    for msg in ('', 'abc', 'sdfdlfjskljsdfjfkldsjklfdsjkfldsljkdfs'):
-        lib = hashlib.new('ripemd160', msg)
-        assert ripemd160(msg).digest() == lib.digest()
+        for msg in ('', 'abc', 'sdfdlfjskljsdfjfkldsjklfdsjkfldsljkdfs'):
+            lib = hashlib.new('ripemd160', msg)
+            assert ripemd160(msg).digest() == lib.digest()
 
+if __name__ == '__main__':
+    unittest.main()
